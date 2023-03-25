7d85c98b9891c9ed88f285e149c7f535cc56c5ad
maganjoken <111058600+maganjoken@users.noreply.github.com>
2023-03-25 12:38:02 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
 # install puppet-lint -v 2.5.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+class flask_installation {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py',
+  }
 }
+
+include flask_installation
